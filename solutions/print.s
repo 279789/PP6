5bf8c5ff4bafb3866326af29c03f586c1e2b9bd1
Filz <phil.reinartz@gmx.de>
2025-05-23 16:14:43 +0200

    asm excicse

---
+++ solutions/print.s
@@ -0,0 +1,19 @@
+.section .data
+msg: .ascii "This is an professional environment.\n"
+len = . - msg
+
+.section .text
+.globl _start
+
+_start:
+movl $4, %eax	#syscall in write mode
+movl $1, %ebx   # file director 1 (where the msg should be writen 1 = stdout = terminal)
+movl $msg, %ecx #from where the message should be written, in our case it's the address of the first char from the Message.
+movl $len, %edx #size of the message, thats needed, so that there is an definition of how long shauld be written.
+int $0x80       #execution of the syscall
+
+
+movl $1, %eax   #syscall for exiting the prozess
+movl $0 ,%ebx   # giving status 0 to the parentprocess
+int $0x80
+
